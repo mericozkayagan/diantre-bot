aa5bb2cc3e0fbe51d1950bb90252d98b20486f41
Meric Ozkayagan <meric.ozkayagan@navlungo.com>
2023-02-26 22:39:12 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mericozkayagan/diantre-bot
+
+go 1.16
+
+require (
+	github.com/bwmarrin/discordgo v0.27.0
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func init(){
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	Token := os.Getenv("DISCORD_TOKEN")
+	if Token == "" {
+		log.Fatal("Unauthorized: No token present")
+	}
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+type Diantre struct {
+	Name string `json: "name"`
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "!diantre" {
+
+		fmt.Println("Greetings from Diantre Bot !!!")
+	}
+
+	if m.Content == "!random" {
+
+		fmt.Println("Something random happens !!!")
+	}
+
+	if m.Content == "!diantre play" {
+
+		fmt.Println("Plays something...")
+	}
+}
